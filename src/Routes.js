be1c1c67ab1090a0d8c72727a587a178030fd32a
Suzannah Barnes <suzannah.barnes@bjss.com>
2023-04-12 14:55:12 +0100

    setup application

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router, Link, Route, } from 'react-router-dom'
+
+const Routes = () => {
+    <Router>
+        <Link>
+        {routes.map((route, index)=> (
+            <Route 
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            >
+                <route.Component />
+            </Route>
+        ))}
+        </Link>
+    </Router>
+}
+
+export default Routes;
