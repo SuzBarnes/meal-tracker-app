e004ba204495df27e792d22d8a349d61358a6d85
Suzannah Barnes <suzannah.barnes@bjss.com>
2023-04-13 12:00:17 +0100

    updated the useMeals useEffect

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import {useMeals} from '../meals'
 
 export const HomePage = () => {
 return(
---
+++ src/meals/useMeals.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from 'react';
+
+export const useMeals = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const loadMeals = async () => {
+            setIsLoading(true);
+            const response = await fetch('/meals');
+            const meals = await response.json();
+            setMeals(meals);
+            setIsLoading(false);
+        }
+        loadMeals();
+    });
+
+    return {isLoading, meals, setMeals};
+}
